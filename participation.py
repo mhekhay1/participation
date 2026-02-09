d67e4226217c3f2b5291084aefc02795033a405d
mhekhay1 <nakasky255@gmail.com>
2026-02-08 20:54:19 -0500

    Update comment for clarity on random integers list

--- participation.py
+++ participation.py
@@ -1,6 +1,6 @@
 import random
 
-# create a list of 100 random integers from 1 to 10 (inclusive)
+#list of 100 random integers from 1 to 10 (inclusive)
 nums = [random.randint(1, 10) for _ in range(100)]
 
 # print the sum of the list
